ab75d2864ec8e6ce224cd6bfe1e5f00da15584fc
Antonio Brown <abrownjr12@outlook.com>
2024-03-02 16:44:40 -0600

    new commit

--- guessing-game.js
+++ guessing-game.js
@@ -1,8 +1,8 @@
 findOut.addEventListener("click", function () {
   Win.style.color = "blue";
-  Win.textContent = "You Win!"
-  Lose.textContent = "You Lose..."
+  Win.textContent = "You Win!";
+  Lose.textContent = "You Lose...";
   Lose.style.color = "red";
+  Win.style.backgroundColor = "gold";
+  Lose.style.backgroundColor = "silver";
 });
-
-
