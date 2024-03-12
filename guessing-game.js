6ed4e89309cdd1e47ce74ef35032318e4ef54989
Antonio Brown <135006436+ab-dani@users.noreply.github.com>
2024-03-11 19:47:37 -0500

    Update guessing-game.js

--- guessing-game.js
+++ guessing-game.js
@@ -2,16 +2,33 @@ document.getElementById("Win").style.display = "none";
 document.getElementById("Lose").style.display = "none";
 
 findOut.addEventListener("click", function () {
-  Win.textContent = "";
-  Win.style.color = "blue";
-  Win.textContent = "You Win!";
-  Lose.textContent = "You Lose...";
-  Lose.style.color = "red";
-  Win.style.backgroundColor = "gold";
-  Lose.style.backgroundColor = "silver";
-  document.getElementById("Win").style.display = "block";
 
-  findOut.addEventListener("click", function () {
+  
+  alert("Lets Play A Game");
+
+  confirm("Its a guessing game!");
+
+  let g = prompt("Guess a number...");
+ 
+  let guess = parseInt(g);
+  
+  if (guess === 12) {
+    alert("You got it!");
+    Win.style.color = "blue";
+    Win.textContent = "You Win!";
+    Win.style.backgroundColor = "gold";
+    document.getElementById("Win").style.display = "block";
+    document.getElementById("Lose").style.display = "none";
+    
+  } else {
+    alert("No!");
+    Lose.textContent = "You Lose...";
+    Lose.style.color = "red";
+    Lose.style.backgroundColor = "silver";
     document.getElementById("Lose").style.display = "block";
-  });
+    document.getElementById("Win").style.display = "none";
+    
+  }
+
+  alert("Thanks for playing!");
 });
